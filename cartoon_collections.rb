a17f090206653f9a77ed3169b7fc3bf0c5907084
james wu <jwu_nyc@yahoo.com>
2020-06-28 15:54:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,9 @@ p square_array(y)
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  planeteer_calls.map do |x|
+    x*x
+  end
 end
 
 def long_planeteer_calls(planeteer_calls)
