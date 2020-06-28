c91743373dfe5721a2ffd1b297b81fb18b69af07
james wu <jwu_nyc@yahoo.com>
2020-06-28 16:24:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def summon_captain_planet(planeteer_calls)
 
 end
 calls = ["earth", "wind", "fire", "water", "heart"]
+calls1 = ["earth", "wind", "fire", "water", "heart"]
 #p summon_captain_planet(calls)
 
 def long_planeteer_calls(planeteer_calls)
