0650dbed5eafe31a836ff5da891f3fc3fae50a2f
james wu <jwu_nyc@yahoo.com>
2020-06-28 16:46:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,3 +37,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.select {|calls|valid_calls.includes?(calls)}
 end
+
+p find_valid_calls(calls2)
